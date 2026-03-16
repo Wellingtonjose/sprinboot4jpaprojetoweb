7b18089ad8e5183820079190438ccdc213696d32
wellington <wellingtoneeee@gmail.com>
2026-03-16 11:39:20 -0300

    Order, Instant, ISO 8601- PARTE 1

--- src/main/java/com/projetoweb/segundo_projeto/entities/User.java
+++ src/main/java/com/projetoweb/segundo_projeto/entities/User.java
@@ -1,12 +1,15 @@
 package com.projetoweb.segundo_projeto.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_user")
@@ -19,6 +22,8 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	@OneToMany(mappedBy = "client")
+	List<Order> orders = new ArrayList<>();
 	public User() {
 		
 	}
@@ -60,6 +65,11 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
