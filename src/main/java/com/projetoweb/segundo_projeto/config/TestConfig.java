616b761489e6af43e173b4b60032bc25d341252b
wellington <wellingtoneeee@gmail.com>
2026-03-18 14:01:06 -0300

    OrderStatus enum

--- src/main/java/com/projetoweb/segundo_projeto/config/TestConfig.java
+++ src/main/java/com/projetoweb/segundo_projeto/config/TestConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.projetoweb.segundo_projeto.entities.Order;
 import com.projetoweb.segundo_projeto.entities.User;
+import com.projetoweb.segundo_projeto.entities.enums.OrderStatus;
 import com.projetoweb.segundo_projeto.repositories.OrderRepository;
 import com.projetoweb.segundo_projeto.repositories.UserRepository;
 
@@ -30,9 +31,9 @@ public class TestConfig implements CommandLineRunner {
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 	
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAD,u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1); 
 		userRepository.saveAll(List.of(u1, u2));
 		useOrderRepository.saveAll(List.of(o1,o2,o3));
 	}
